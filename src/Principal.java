7c1250af459b5be1c382db5b488bc9fb7cb03934
Estebanmg12 <95311965+Estebanmg12@users.noreply.github.com>
2024-10-11 11:54:03 -0300

    Challenge Conversor de Monedas

---
+++ src/Principal.java
@@ -0,0 +1,88 @@
+
+import com.google.gson.JsonSyntaxException;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+        int opcionElegida = 0;
+
+        ConsultaMoneda consulta = new ConsultaMoneda();
+
+        CalculoMoneda calculos = new CalculoMoneda(consulta);
+        GeneradorDeArchivos generador = new GeneradorDeArchivos();
+
+        List<String> respuestas = new ArrayList<>();
+
+        String menu = """
+                \n***************************************************
+                 Sea bienvenido/a al Conversor de Monedas =)
+                
+                1) Dólar ==>> Peso argentino
+                2) Peso argentino ==>> Dólar
+                3) Dólar ==>> Real brasileño
+                4) Real brasileño ==>> Dólar
+                5) Dólar ==>> Peso colombiano
+                6) Peso colombiano ==>> Dólar
+                
+                7) Salir
+                ***************************************************
+                """;
+
+        while (opcionElegida != 8) {
+            try {
+                System.out.println(menu);
+                opcionElegida = Integer.parseInt(lectura.nextLine());
+
+                LocalDateTime myDateObj = LocalDateTime.now();
+                DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+                String formattedDate = myDateObj.format(myFormatObj);
+
+                switch (opcionElegida) {
+                    case 1:
+                        calculos.almacenarValores("USD", "ARS");
+                        respuestas.add(formattedDate + " - " + calculos.obtenerMensajeRespuesta());
+                        break;
+                    case 2:
+                        calculos.almacenarValores("ARS", "USD");
+                        respuestas.add(formattedDate + " - " + calculos.obtenerMensajeRespuesta());
+                        break;
+                    case 3:
+                        calculos.almacenarValores("USD", "BRL");
+                        respuestas.add(formattedDate + " - " + calculos.obtenerMensajeRespuesta());
+                        break;
+                    case 4:
+                        calculos.almacenarValores("BRL", "USD");
+                        respuestas.add(formattedDate + " - " + calculos.obtenerMensajeRespuesta());
+                        break;
+                    case 5:
+                        calculos.almacenarValores("USD", "COP");
+                        respuestas.add(formattedDate + " - " + calculos.obtenerMensajeRespuesta());
+                        break;
+                    case 6:
+                        calculos.almacenarValores("COP", "USD");
+                        respuestas.add(formattedDate + " - " + calculos.obtenerMensajeRespuesta());
+                        break;
+
+                    case 7:
+                        break;
+                    default:
+                        System.out.println("Ingrese una opción válida");
+                }
+            } catch (JsonSyntaxException | NullPointerException e) {
+                System.out.println("Error. Ingrese solo códigos de moneda válidos.");
+            } catch (NumberFormatException | InputMismatchException e) {
+                System.out.println("Error. Ingrese un valor numérico válido.");
+            }
+        }
+        generador.guardarJson(respuestas);
+
+        System.out.println("Finalizando programa");
+    }
+}
